75f5411f2fee0387856c5917d22f3b8238420aa8
abdusami1 <taqvoqiling7@gmail.com>
2025-05-13 16:32:05 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,22 @@
-from aiogram import Bot, Dispatcher, types
-from aiogram.types import Message
-from aiogram.dispatcher.webhook import SendMessage
-from aiogram.utils.executor import start_webhook
-import logging
 import os
+from aiogram import Bot, Dispatcher, types
+from aiogram.utils.executor import start_webhook
+from aiohttp import web
 
 API_TOKEN = os.getenv("BOT_TOKEN")
-
-WEBHOOK_HOST = 'https://tasbeh-bot.onrender.com'
-WEBHOOK_PATH = f'/webhook/{API_TOKEN}'
+WEBHOOK_HOST = os.getenv("RENDER_EXTERNAL_URL")
+WEBHOOK_PATH = "/webhook"
 WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
 
-WEBAPP_HOST = '0.0.0.0'
-WEBAPP_PORT = int(os.environ.get('PORT', 5000))
+WEBAPP_HOST = "0.0.0.0"
+WEBAPP_PORT = int(os.getenv("PORT", 8000))
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
-logging.basicConfig(level=logging.INFO)
-
-@dp.message_handler(commands=['start'])
-async def start_handler(message: Message):
-    return SendMessage(message.chat.id, "Assalomu alaykum, tasbeh botga xush kelibsiz!")
+@dp.message_handler(commands=["start"])
+async def start_handler(message: types.Message):
+    await message.answer("Assalomu alaykum, Tasbeh botga xush kelibsiz!")
 
 async def on_startup(dp):
     await bot.set_webhook(WEBHOOK_URL)
@@ -29,12 +24,13 @@ async def on_startup(dp):
 async def on_shutdown(dp):
     await bot.delete_webhook()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     start_webhook(
         dispatcher=dp,
         webhook_path=WEBHOOK_PATH,
         on_startup=on_startup,
         on_shutdown=on_shutdown,
+        skip_updates=True,
         host=WEBAPP_HOST,
         port=WEBAPP_PORT,
     )
